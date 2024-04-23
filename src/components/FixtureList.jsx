9a296a1ce1f78c120d1f99bfdcdfd3336728524f
Jesús Ramon Martinez Cervantes <martinezcjr1@gmail.com>
2024-04-23 12:32:51 -0700

    Crea lógica de paginación en FixtureList.jsx

--- src/components/FixtureList.jsx
+++ src/components/FixtureList.jsx
@@ -1,19 +1,62 @@
+import { useEffect, useState } from "react";
 import FixtureDetails from "./FixtureDetails";
 import './styles/FixtureList.css'
 
+const PAGE_SIZE = 20;
 const FixtureList = ({ fixtures }) => {
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPageFixtures, setCurrentPageFixtures] = useState([]);
+
+  useEffect(() => {
+    setTotalPages(Math.ceil(fixtures?.length / PAGE_SIZE));
+    setCurrentPageFixtures(getCurrentPageFixtures());
+  }, [fixtures, currentPage]);
+
+  const getCurrentPageFixtures = () => {
+    const startIndex = (currentPage - 1) * PAGE_SIZE;
+    const endIndex = Math.min(startIndex + PAGE_SIZE, fixtures?.length);
+    return fixtures.slice(startIndex, endIndex);
+  };
+
+  const goToPage = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  const goToPreviousPage = () => {
+    goToPage(currentPage - 1);
+  };
+
+  const goToNextPage = () => {
+    goToPage(currentPage + 1);
+  };
+
+  if (!fixtures || fixtures?.length === 0) {
+    return <div className="Warning__API">Por ahora no hay datos disponibles!</div>;
+  }
 
   return (
     <div className="fixture-list">
      
       <div className="fixture__card">{
-      fixtures?.map((fixture, index) => (
+      currentPageFixtures?.map((fixture, index) => (
         <div className="fixture__card-container" key={index}>
           <FixtureDetails fixture={fixture} />
           
         </div>
       ))}
+      </div>
+      <div className="pagination">
+        <button onClick={goToPreviousPage} disabled={currentPage === 1}>
+           Previous
+        </button>
+          <span>{`Page ${currentPage} of ${totalPages}`}</span>
+          <button onClick={goToNextPage} disabled={currentPage === totalPages}>
+            Next
+        </button>
       </div>
     </div>
   );
