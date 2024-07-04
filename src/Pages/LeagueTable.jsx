962fae06c6029d141d589204e3bfecbfbcce6fa5
Jesús Ramon Martinez Cervantes <martinezcjr1@gmail.com>
2024-07-04 11:06:28 -0700

    Agrega scroll a LeagueTable para smartphone

--- src/Pages/LeagueTable.jsx
+++ src/Pages/LeagueTable.jsx
@@ -44,14 +44,14 @@ const LeagueTable = ({ fixtures }) => {
                 <table className="leaguetable__table01">
                     <thead className="leaguetable__head01"> 
                         <tr className="leaguetable__tr-head01">
-                            <th></th>
-                            <th>Equipo</th>
+                            <th className='leaguetable__logo'></th>
+                            <th className='leaguetable__th01'>Equipo</th>
                         </tr>
                     </thead>
                     <tbody className="leaguetable__body01">
                         {parsedTable.map((team, index) => (
                             <tr className="leaguetable__tr-body01" key={index}>
-                                <td>
+                                <td className='td__logo'>
                                     <img src={teamLogos[team.Equipo]} alt={`${team.Equipo} logo`} />
                                 </td>
                                 <td>{team.Equipo}</td>
@@ -59,38 +59,37 @@ const LeagueTable = ({ fixtures }) => {
                         ))}
                     </tbody>
                 </table>
-
-                <table className="leaguetable__table">
-                    <thead className="leaguetable__head"> 
-                        <tr className="leaguetable__tr-head">
-                            <th>JJ</th>
-                            <th>JG</th>
-                            <th>JE</th>
-                            <th>JP</th>
-                            <th>Pts</th>
-                            <th>DG</th>
-                            <th>GF</th>
-                            <th>GC</th>
-                        </tr>
-                    </thead>
-                    <tbody className="leaguetable__body">
-                        {parsedTable.map((team, index) => (
-                            <tr className="leaguetable__tr-body" key={index}>
-                                <td>{team.JJ}</td>
-                                <td>{team.JG}</td>
-                                <td>{team.JE}</td>
-                                <td>{team.JP}</td>
-                                <td>{team.Pts}</td>
-                                <td>{team.DG}</td>
-                                <td>{team.GF}</td>
-                                <td>{team.GC}</td>
+                <div className='leaguetable__table-scroll'>
+                    <table className="leaguetable__table">
+                        <thead className="leaguetable__head"> 
+                            <tr className="leaguetable__tr-head">
+                                <th>JJ</th>
+                                <th>JG</th>
+                                <th>JE</th>
+                                <th>JP</th>
+                                <th>Pts</th>
+                                <th>DG</th>
+                                <th>GF</th>
+                                <th>GC</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody className="leaguetable__body">
+                            {parsedTable.map((team, index) => (
+                                <tr className="leaguetable__tr-body" key={index}>
+                                    <td>{team.JJ}</td>
+                                    <td>{team.JG}</td>
+                                    <td>{team.JE}</td>
+                                    <td>{team.JP}</td>
+                                    <td>{team.Pts}</td>
+                                    <td>{team.DG}</td>
+                                    <td>{team.GF}</td>
+                                    <td>{team.GC}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </section>
-
-          
         </section>
     )
 }
